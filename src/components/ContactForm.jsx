34c3fe7b60051976c3e5f1020c2d20a58798d26a
Grover Soans <groverpsoans@gmail.com>
2024-10-21 16:59:14 -0500

    Contact form implemented

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,120 @@
+import emailjs from "@emailjs/browser"
+import { useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+
+const ContactForm = () => {
+    const [formData, setFormData] = useState({
+        name:"",
+        email:"",
+        message:"",
+    })
+
+    const [error, setError] = useState({})
+
+    const [isSending, setIsSending] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+       e.preventDefault();
+       const validationErrors = validate();
+       if(Object.keys(validationErrors).length > 0){
+        setError(validationErrors)
+       } else ({});
+       setIsSending(true)
+
+       emailjs
+       .send(
+            "service_nkkttoj",
+            "template_v08yo2n",
+            formData,
+            "kco68uhOSTnRvoDDz"
+       )
+       .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        toast.success("Message sent successfully");
+        setFormData({name:"", email:"", message:""})
+       })
+       .catch((error) =>{
+        console.log("FAILED...", error)
+        toast.error("Failed to send message. Please try again")
+       })
+       .finally(()=>{
+        setIsSending(false)
+       })
+    }
+
+    const validate = () => {
+        let error = {};
+        if(!formData.name) error.name = "Name is required";
+        if(!formData.email) {
+            error.email = "Email is required";
+        } else if(!/\S+@\S+\.\S+/.test(formData.email)) {
+            error.email = "Email is invalid"
+        }
+        if (!formData.message) error.message = "Message is required"
+        return error;
+    }
+  return (
+    <div className="mx-auto max-w-3xl p-4" id="contact">
+        <Toaster/>
+        <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
+            Lets Connect
+        </h2>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+                <input type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                placeholder="Name"
+                onChange={handleChange}
+                className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm
+                focus:border-gray-400 focus:outline-none"/>
+                {error.name && (
+                    <p className="text-sm text-pink-700">{error.name}</p>
+                )}
+            </div>
+            <div className="mb-4">
+                <input type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                placeholder="Email"
+                onChange={handleChange}
+                className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm
+                focus:border-gray-400 focus:outline-none"/>
+                {error.email && (
+                    <p className="text-sm text-pink-700">{error.email}</p>
+                )}
+            </div>
+            <div className="mb-4">
+                <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                placeholder="Message"
+                onChange={handleChange}
+                className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm
+                focus:border-gray-400 focus:outline-none" rows="4"/>
+                {error.message && (
+                    <p className="text-sm text-pink-700">{error.message}</p>
+                )}
+            </div>
+            <button type="submit" className={`mb-8 w-full rounded bg-yellow-400 px-4 py-2 text-sm font-semibold text-slate-950 hover:bg-yellow-500 ${
+                isSending ? 'cursor-not-allowed opacity-50' : ""}`}
+                disabled={isSending}>
+                    {isSending ? "Sending..." : "Send"}
+                </button>
+        </form>
+    </div>
+  )
+}
+
+export default ContactForm
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { PROJECTS } from '../constants'
 import Card from './Card'
 

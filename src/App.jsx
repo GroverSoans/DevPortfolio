0681c7cd6ec04ccf26b756f4ca673f8226800e44
Grover Soans <groverpsoans@gmail.com>
2024-10-21 15:49:12 -0500

    hero about navbar implemented

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
+      <Navbar/>
+      <HeroSection/>
+      <About/>
+    </main>
+  )
+}
+
+export default App
